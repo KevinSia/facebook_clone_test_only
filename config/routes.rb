4e0200d809a791cd215ea12eb401314c59302157
KevinSia <kevinsiakeemun@gmail.com>
2016-06-12 23:56:31 +0800

    done model test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,33 @@
 Rails.application.routes.draw do
 
+  get 'statuses/index'
 
+  get 'statuses/show'
 
+  get 'statuses/new'
+
+  get 'statuses/create'
+
+  get 'statuses/edit'
+
+  get 'statuses/update'
+
+  get 'statuses/destroy'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
+  root 'welcome#index'
+
+  resource :sessions, only: [:new, :create, :destroy]
+  resources :statuses
+  resources :users, except: [:index]
+  resources :likes, only: [:create, :destroy]
 end
