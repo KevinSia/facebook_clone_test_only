3b22a0e5c71d0c6b002ec8397a54b75197c79258
KevinSia <kevinsiakeemun@gmail.com>
2016-06-13 00:43:44 +0800

    done controller test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
+  private
+
+  def require_login
+    unless logged_in?
+      redirect_to new_session_path
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,7 @@
 Rails.application.routes.draw do
-
-  get 'statuses/index'
-
-  get 'statuses/show'
-
-  get 'statuses/new'
-
-  get 'statuses/create'
-
-  get 'statuses/edit'
-
-  get 'statuses/update'
-
-  get 'statuses/destroy'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  get 'users/destroy'
-
   root 'welcome#index'
-
-  resource :sessions, only: [:new, :create, :destroy]
+  get 'sign_out' => 'sessions#destroy', as: 'signout'
+  resource :session, only: [:new, :create]
   resources :statuses
   resources :users, except: [:index]
   resources :likes, only: [:create, :destroy]
